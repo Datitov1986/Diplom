242fb1ba0c1db8b0f94cab2d1538ba3238873d61
Dmitri <datitov2009@yandex.ru>
2023-07-02 10:57:34 +0300

    Начал написание кода с пользовательского интерфейса, вывода меню выбора

---
+++ Counter/src/View/GetAnswer.java
@@ -0,0 +1,20 @@
+package View;
+
+import java.util.Scanner;
+
+public class GetAnswer {
+    public static int ChoiceInMenu() {
+        int choice;
+        while (true) {
+            try {
+                Scanner in = new Scanner(System.in);
+                System.out.print("--> ");
+                choice = in.nextInt();
+                break;
+            } catch (Exception ex) {
+                System.out.println("Не верный ввод, необходимо указать число от нуля и выше.");
+            }
+        }
+        return choice;
+    }
+}
---
+++ Counter/src/View/Menu.java
@@ -0,0 +1,11 @@
+package View;
+
+public class Menu {
+    public static void MainMenu(int values) {
+        System.out.println("В моей программе вы можете вносить или считывать ранее внесенные показания" +
+                "различных счетчиков ЖКХ");
+        System.out.println("1. Внести показание");
+        System.out.println("2. Посмотреть ранее внесенные показания");
+        System.out.println("0. Уйти\n");
+    }
+}
